675be73f2532cd4551db115906f7000fbd37a182
amirparvaneh <amirparvaneh138@gmail.com>
2024-09-06 23:30:39 +0330

    [feature] data model completion

--- src/main/java/com/example/ticketting/model/Escalation.java
+++ src/main/java/com/example/ticketting/model/Escalation.java
@@ -6,16 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table
+@Entity
 public class Escalation extends BaseEntity{
     private LocalDateTime escalationDate;
     private Integer newResponseTimeLimit;
